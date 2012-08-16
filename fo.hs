e1bf5d5ad14ee13160c4b42c027c710cad106d60
BenjaminMalley <benjamin.malley@gmail.com>
2012-08-16 00:33:22 -0400

    modified FO data type definitions to be closer to AIMA's

--- fo.hs
+++ fo.hs
@@ -1,15 +1,29 @@
-data Sentence = Connective Sentence Connective Sentence
-	| Quantifier Quantifier [Variable] Sentence
-	| Not Sentence
-	| Predicate String [Term]
-	| Equal Term Term
-	
-data Term = Function String [Term]
-	| Constant String
-	| Variable Variable
-	
-type Variable = String
-data Connective = If | Iff | And | Or
-data Quantifier = ForAll | Exists
+import Debug.Trace
 
-main = putStrLn "Hello"	
+data Sentence = Connective Sentence Connective Sentence
+	| Quantifier Quantifier [Var] Sentence
+	| Not Sentence
+	| Atom Atom deriving (Eq, Read, Show)
+
+data Atom = Predicate Predicate [Term]
+	| Equal Term Term deriving (Eq, Read, Show)
+
+data Term = Function Const [Term]
+	| Const Const
+	| Var Var deriving (Eq, Read, Show)
+
+data Connective = If | Iff | And | Or deriving (Eq, Read, Show)
+
+data Quantifier = ForAll | Exists deriving (Eq, Read, Show)
+
+type Var = String
+type Const = String
+type Function = String
+type Predicate = String
+
+data Subst = Subst Var Const
+
+
+--unify :: Atom -> Atom -> [Atom] -> Maybe Atom
+
+--unifyVar :: Var -> Atom ->
