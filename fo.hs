66e461e3f3c4cf9ea34d1e595509b8b42bb4ab55
Benjamin Malley <benjamin.malley@gmail.com>
2012-08-16 14:18:01 -0400

    cleaned up unification algorithm so that the types are a bit cleaner

--- fo.hs
+++ fo.hs
@@ -1,6 +1,5 @@
 module FO where
 
-import Debug.Trace
 import Data.List (find)
 
 data Sentence = Connective Sentence Connective Sentence
@@ -11,7 +10,7 @@ data Sentence = Connective Sentence Connective Sentence
 data Atom = Predicate Predicate [Term]
 	| Equal Term Term deriving (Eq, Read, Show)
 
-data Term = Function Term [Term] --for typing; should be Function Function [Term]
+data Term = Function Function [Term] 
 	| Const Const
 	| Var Var deriving (Eq, Read, Show)
 
@@ -24,26 +23,32 @@ type Const = String
 type Function = String
 type Predicate = String
 
-data Subst = Subst Term Term --temporary hack to avoid typing issues; should be Subst Var Const
+-- Although a Subst is technically a (Var, Const), the unification algorithm
+-- is sufficient to ensure this.
+type Subst = (Term, Term)
 
 findSubst :: Term -> Maybe [Subst] -> Maybe Term
 findSubst _ Nothing = Nothing
-findSubst x (Just subs) = case find (\(Subst v _) -> x == v) subs of
+findSubst x (Just subs) = case find (\(v, _) -> x == v) subs of
 	Nothing -> Nothing
-	(Just (Subst _ c)) -> Just c
+	(Just (_, c)) -> Just c
 
 unifyList :: [Term] -> [Term] -> Maybe [Subst] -> Maybe [Subst]
 unifyList [] [] theta = theta
 unifyList [] (y:ys) _ = Nothing --return failure for lists of unequal length
-unifyList (x:xs) [] _ = Nothing --confidence that this is right = 50%
+unifyList (x:xs) [] _ = Nothing
 unifyList (x:xs) (y:ys) theta = unifyList xs ys $ unify x y theta
 
+-- Algorithm has some modifications from the one in AIMA for typing reasons:
+-- 1. Handling of lists has been moved off into unifyList.
+-- 2. For Functions x & y, unify checks their equality in place, instead of recurring.
+-- The other approach would have been to make Function = Function Const [Term] but this seemed like cheating.
 unify :: Term -> Term -> Maybe [Subst] -> Maybe [Subst]
 unify _ _ Nothing = Nothing
 unify x y theta | x == y = theta
 unify x@(Var _) y theta = unifyVar x y theta
 unify x y@(Var _) theta = unifyVar y x theta
-unify x@(Function f args) y@(Function f' args') theta = unifyList args args' $ unify f f' theta
+unify x@(Function f args) y@(Function f' args') theta = unifyList args args' $ if f == f' then theta else Nothing
 unify _ _ _ = Nothing
 
 unifyVar :: Term -> Term -> Maybe [Subst] -> Maybe [Subst]
@@ -51,10 +56,14 @@ unifyVar v x theta@(Just subs) = case findSubst v theta of
 	(Just val) -> unify val x theta
 	Nothing -> case findSubst x theta of
 		(Just val') -> unify v val' theta
-		Nothing -> if occurCheck v x then Nothing else Just $ (Subst v x) : subs
+		Nothing -> if occurCheck v x then Nothing else Just $ (v, x) : subs
 
 occurCheck :: Term -> Term -> Bool
-occurCheck v (Var _) = False
-occurCheck v (Const _) = False
-occurCheck v (Function f args) = elem v args
+occurCheck v (Function f (arg:args)) = checkList v args where
+	checkList v [] = True
+	checkList v (x:xs) = (occurCheck v x) && (checkList v xs)
+occurCheck v x = v == x	
 
+-- from AIMA, pg 277
+-- returns Just [(Var "x", Const "Jane")]
+main = putStrLn $ show $ unify (Function "Knows" [Const "John", Var "x"]) (Function "Knows" [Const "John", Const "Jane"]) (Just [])
